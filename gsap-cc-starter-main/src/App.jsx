0cc6cd9441a6176cf15a84070ec9db00fa939aa8
nyapo1 <felixnyapo1@gmail.com>
2025-01-28 13:27:46 +0300

    Add the 3D phone that the use can scroll and view in 3D

--- gsap-cc-starter-main/src/App.jsx
+++ gsap-cc-starter-main/src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar";
 import Highlights from "./components/Highlights";
 import Hero from "./components/Hero";
+import Model from "./components/Model";
 
 
 const App = () => {
@@ -9,7 +10,7 @@ const App = () => {
     <Navbar/>
     <Hero/>
     <Highlights/>
-
+    <Model/>
    </main>
   );
 };
